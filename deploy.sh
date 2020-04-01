dd27334264261bc1939dd2e1ac576087c934cc5f
lijianglu <bruceleev@163.com>
2020-04-01 14:47:44 +0800

    直接修改shell文件

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+export PROJ_PATH=/root/.jenkins/workspace/Order/order
 
 # 输入你的环境上tomcat的全路径
-# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+export TOMCAT_APP_PATH=/home/software/apache-tomcat/tomcat8-linux-9006/apache-tomcat-8.5.42
 
 ### base 函数
 killTomcat()
